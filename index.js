9914031c3d1af21f923b5fab4aa1e5d17bbafc35
Kiranmayi-s <83873537+Kiranmayi-s@users.noreply.github.com>
2021-07-19 12:43:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 function display(){
     let city = document.getElementById('myText').value;
     document.getElementById('myText').value ="";
+    document.getElementById('body').innerHTML ="";
 fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=51a0780cc3e7ff809ccc53e63dba4764")
 .then(function (Data1){
     return Data1.json()
@@ -12,16 +13,18 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=51a0780c
     let temp = data.main.temp;
     let description = data['weather'][0]['description'];
     let icon = "https://openweathermap.org/img/w/"+data.weather[0].icon+".png";
+    let feels = data.main.feels_like;
 
     document.getElementById('city').innerHTML = "City :  " + (city.charAt(0).toUpperCase()+ city.slice(1));
     document.getElementById('temp').innerHTML = "Temperature : " + (Math.floor(temp-273.1)+"°C");
     document.getElementById('desc').innerHTML = "Weather condition :  " + (description.charAt(0).toUpperCase()+ description.slice(1));
+    document.getElementById('feelslike').innerHTML = "Feels Like : " + (Math.floor(feels-273.1)+"°C");
     console.log(icon);
     var img = document.createElement('img'); img.setAttribute("id","image")
     img.src = icon;
     document.getElementById('body').appendChild(img);
 })
 .catch(function(error){
-    console.log(error);
+    alert('Please enter a valid city name')
 })
 }
